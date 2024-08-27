14719b5754b0206239f2a5368bb7d007e94f3e17
wildcat2k21 <djerse12@mail.ru>
2024-08-28 01:46:17 +0300

    notify commit 14

--- index.js
+++ index.js
@@ -63,7 +63,8 @@ app.post('/user', async (req, res) => {
         //оповещение о новом пользователе
         await BotService.NOTIFY([{
             id: ADMIN_ID,
-            message: `У вас новый пользователь "${body.full_name}" ℹ️/n/n
+            message: `У вас новый пользователь:/n/n
+            👤"${body.full_name}"/n/n
             👥 Всего пользователей: ${(totalParticipants + 1)}`
         }]);
 
