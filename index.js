016524f7b799f0eca2a6470b801740154cdf18b1
origami <djerse12@mail.ru>
2024-11-18 02:18:25 +0300

    update fix 3

--- index.js
+++ index.js
@@ -1270,12 +1270,16 @@ app.listen(PORT, '0.0.0.0', async () => {
     console.clear();
 
     //инициаилзация
-    const config_json = await import("./config.json", {
-        assert: { type: "json" },
-    });
+    // const config_json = await import("./config.json", {
+    //     assert: { type: "json" },
+    // });
+
+    const config_json = JSON.parse(await fs.readFile(new URL('./config.json', import.meta.url)));
 
     //конфигурация сообщений
     config = {...config_json.default};
+
+    console.log(config_json);
 
     //инициализация XUI API
     await XUI_API.InitXrayConfig();
